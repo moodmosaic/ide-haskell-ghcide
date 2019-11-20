915c1f8b65648ed181dc5f161886bfbbcef1a0db
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2019-11-20 14:31:08 +0100

    apm init -p ide-haskell-ghcide --syntax javascript

---
+++ spec/ide-haskell-ghcide-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import IdeHaskellGhcideView from '../lib/ide-haskell-ghcide-view';
+
+describe('IdeHaskellGhcideView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
