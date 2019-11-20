4adf0aaba8253ed918c42a6cb0bb7332ed230e4a
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2019-11-20 15:07:23 +0100

    Housekeeping

--- spec/ide-haskell-ghcide-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import IdeHaskellGhcideView from '../lib/ide-haskell-ghcide-view';
-
-describe('IdeHaskellGhcideView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
